c2f8870fe6439b43c633f3290e712661bdd9a2e7
jbuechs <janina.buechner@gmail.com>
2015-08-08 17:16:05 -0400

    Sidebar toggle hidden on small screen

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-//Code for the Google Map API
-
+// Google Map API
 function initialize() {
         var mapOptions = {
 
@@ -13,3 +12,10 @@ function initialize() {
             mapOptions);
       }
       google.maps.event.addDomListener(window, 'load', initialize);
+
+// Sidebar toggling on/off view on small screen
+$(document).ready(function() {
+  $('[data-toggle=offcanvas]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+  });
+});

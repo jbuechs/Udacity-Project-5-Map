30a2a0c2494d7236b8fef968f70867015745bf4f
jbuechs <janina.buechner@gmail.com>
2015-08-09 13:25:31 -0400

    Load map markers and info window

--- js/app.js
+++ js/app.js
@@ -1,32 +1,64 @@
-// Google Map API
-function initialize() {
-  var mapOptions = {
-
+var initialize = function(){
+  mapOptions = {
   // Default map center: Madrid, Spain
     center: { lat: 40.4213473, lng: -3.7005081},
 
   // Default zoom
     zoom: 12,
   };
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-}
+  // Create map
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  // Load markers
+  mapView.loadMarkers();
+};
 
-function loadScript() {
+var loadScript = function(){
   var script = document.createElement('script');
   script.type = 'text/javascript';
   script.onerror = function() {
-    console.log('Map loading error');
     $('#map-canvas').text('Error loading Google Map');
   };
   script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
       '&signed_in=true&callback=initialize';
   document.body.appendChild(script);
-}
+};
 
-google.maps.event.addDomListener(window, 'load', initialize);
+var mapView = {
+  init: function(){
+      window.onload = loadScript;
+  },
+  render: function(){
 
+  },
+  // Loading markers on the map
+  loadMarkers: function(){
+    var marker, myLatlng, i, locArray;
+    // Create InfoWindow
+    var infowindow = new google.maps.InfoWindow({
+      content: 'Hello, world!',
+    });
+    locArray = neighborhood.locations;
+    this.markersArray = [];
+    for (i = 0; i < locArray.length; i++) {
+      myLatlng = new google.maps.LatLng(locArray[i].coord.lat, locArray[i].coord.lng);
+      marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: locArray[i].name,
+      });
+      // Add click event listener to InfoWindow
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,this);
+      });
+      this.markersArray.push(marker);
+    }
+  },
+  toggleBounce: function() {
 
+  },
+};
 
+mapView.init();
 
   // Sidebar toggling on/off view on small screen
   $(document).ready(function() {
---
+++ js/locations.js
@@ -0,0 +1,35 @@
+var neighborhood = {
+	locations: [
+	{
+		'name': 'Apartment #1',
+		'address': 'Calle Luis de Góngora, 14',
+		'coord': {lat: 40.423345, lng: -3.696595},
+		'type': 'apartment',
+		'desc': 'My first apartment in Madrid.',
+	},
+	{
+		'name': 'Apartment #2',
+		'address': 'Calle de Luciente, 14',
+		'coord': {lat: 40.410548, lng: -3.711406},
+		'type': 'apartment',
+		'desc': 'My second apartment in Madrid.',
+	},
+	{
+		'name': 'Apartment #3',
+		'address': 'Calle Hortaleza, 15',
+		'coord': {lat: 40.421276, lng: -3.700484},
+		'type': 'apartment',
+		'desc': 'My third apartment in Madrid. Where I lived when I had Holiday.',
+	},
+/*
+	{
+		'name': '',
+		'address': '',
+		'coord': {lat: 0, lng: 0},
+		'type': '',
+		'desc': '',
+	},
+*/
+	]
+
+};

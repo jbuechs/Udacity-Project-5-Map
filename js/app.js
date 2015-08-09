b4ffa769b360faf0416d4e631c39382682c6481f
jbuechs <janina.buechner@gmail.com>
2015-08-09 09:42:30 -0400

    Add error handling to map

--- js/app.js
+++ js/app.js
@@ -1,21 +1,36 @@
 // Google Map API
 function initialize() {
-        var mapOptions = {
+  var mapOptions = {
 
-        // Default map center: Madrid, Spain
-          center: { lat: 40.4213473, lng: -3.7005081},
+  // Default map center: Madrid, Spain
+    center: { lat: 40.4213473, lng: -3.7005081},
 
-        // Default zoom
-          zoom: 12
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+  // Default zoom
+    zoom: 12,
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
 
-// Sidebar toggling on/off view on small screen
-$(document).ready(function() {
-  $('[data-toggle=offcanvas]').click(function() {
-    $('.row-offcanvas').toggleClass('active');
-  });
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.onerror = function() {
+    console.log('Map loading error');
+    $('#map-canvas').text('Error loading Google Map');
+  };
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+      '&signed_in=true&callback=initialize';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript;
+
+
+
+
+  // Sidebar toggling on/off view on small screen
+  $(document).ready(function() {
+    $('[data-toggle=offcanvas]').click(function() {
+      $('.row-offcanvas').toggleClass('active');
+    });
 });

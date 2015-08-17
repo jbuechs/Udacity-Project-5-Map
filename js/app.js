6c40272d5ce1e7b44e776f9100b4c211de0ae26c
jbuechs <janina.buechner@gmail.com>
2015-08-17 10:35:08 -0400

    foursquare ajax request for rating - not working :(

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+//Foursquare API
+var CLIENT_ID = '2R1R4RS5FIXI3P4MO04O4QCNFLXDUL5WUOXSVGG4SV3N2AM5';
+var CLIENT_SECRET = 'NUN3OJIAXIUR2JUKRR12MLXH0PQND0IOM1QINSDED0HNSWCU';
+
 // Initializes and creates the google map
 var initialize = function(){
   mapOptions = {
@@ -5,7 +9,7 @@ var initialize = function(){
     center: { lat: 40.4213473, lng: -3.7005081},
 
   // Default zoom
-    zoom: 12,
+    zoom: 14,
   };
   // Create map
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -13,7 +17,7 @@ var initialize = function(){
   mapViewModel.loadMarkers();
 };
 
-// Loads the script tag into end of body of html
+// Loads the script tag for Google Map API into end of body of html
 var loadScript = function(){
   var script = document.createElement('script');
   script.type = 'text/javascript';
@@ -25,12 +29,14 @@ var loadScript = function(){
   document.body.appendChild(script);
 };
 
+// Clicked marker bounces one time
 function stopAnimation(marker) {
   setTimeout(function() {
     marker.setAnimation(null);
   }, 700);
 }
 
+// Google Map API
 var mapViewModel = {
   init: function(){
       window.onload = loadScript;
@@ -38,12 +44,13 @@ var mapViewModel = {
   // Loading markers on the map
   loadMarkers: function(){
     var marker, myLatlng, i, locArray;
-    // Create InfoWindow
+    // Create one InfoWindow
     this.infowindow = new google.maps.InfoWindow({
-      content: 'Hello, world!',
+      content: 'There was an error loading the content.',
     });
     locArray = AVM.displayLoc();
     for (i = 0; i < locArray.length; i++) {
+      //Create a marker for each location object in the model
       myLatlng = new google.maps.LatLng(locArray[i].coord().lat, locArray[i].coord().lng);
       marker = new google.maps.Marker({
         position: myLatlng,
@@ -70,7 +77,7 @@ $(document).ready(function() {
     $('[data-toggle=offcanvas]').click(function() {
       $('.row-offcanvas').toggleClass('active');
     });
-  //Change active state for sidebar links
+  // Change active state for sidebar links when clicked
     $('li').click(function() {
       items = document.getElementsByTagName('li');
       for (var i = 0; i < items.length; i++) {
@@ -84,23 +91,40 @@ $(document).ready(function() {
 
 // Create location object
 var locObj = function(data) {
+  var self = this;
   this.name = ko.observable(data.name);
   this.address = ko.observable(data.address);
   this.coord = ko.observable(data.coord);
   this.type = ko.observable(data.type);
   this.desc = ko.observable(data.desc);
+  this.venue_id = data.venue_id;
+  $.ajax({
+    url: 'https://api.foursquare.com/v2/venues/' + this.venue_id,
+    dataType: 'json',
+    data: 'client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=20140806',
+    async: true,
+    hotspot: self,
+    success: function(data) {
+      console.log(data.response.venue.rating, self);
+      this.hotspot.rating = data.response.venue.rating;
+    }
+  });
+  console.log(this.name(), this.rating);
   this.contentString = ko.computed(function(){
+    var self = this;
     var beginHTML = '<div id="content">'+
       '<div id="siteNotice">'+
       '</div>';
-    var headerString = '<h1 id="firstHeading" class="firstHeading">' +
+    var headerString = '<h4 id="firstHeading" class="firstHeading">' +
       this.name() +
-      '</h1>' +
+      '</h4>' +
       '<div id="bodyContent">';
+    var addressString = '<p><b>Address:</b> ' + this.address() + '</p>';
+    var descString = '<p><b>Description:</b> ' + this.desc() + '</p>';
+    var ratingString = '<p><b>Foursquare rating:</b> <span id="fourRating">' + this.rating + '</span></p>';
     var endHTML = '</div>'+
       '</div>';
-//      console.log(beginHTML + headerString + endHTML);
-      return beginHTML + headerString + endHTML;
+      return beginHTML + headerString + addressString + descString + ratingString + endHTML;
   }, this);
 };
 
@@ -108,7 +132,7 @@ function AppViewModel() {
   var self = this;
   this.koLocArray = ko.observableArray([]);
   var locationArray = neighborhood.locations;
-// Initialize KO array of locations
+// Parsing neighborhood model to create KO array of locations
   locationArray.forEach(function(loc) {
     self.koLocArray.push(new locObj(loc));
   });
@@ -133,7 +157,7 @@ function AppViewModel() {
       });
     }
   }, this);
-  // Make clicked elment in list view be clicked on the marker
+  // Make clicked element in list view be clicked on the marker
   this.clickMarker = function() {
     google.maps.event.trigger(this.marker, 'click');
   };
